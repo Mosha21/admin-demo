83ac2a7283807cd01ca7d543a1b209e222ce49cd
Mosha21 <bernardo_mm@hotmail.com>
2023-09-22 18:03:13 -0600

    Feat: Remove calendar, integration and docs

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -46,38 +46,6 @@ const routes = [
 
   {
     path: "", //no url needed as this has submenu
-    icon: <DocumentDuplicateIcon className={`${iconClasses} inline`} />, // icon component
-    name: "Pages", // name that appear in Sidebar
-    submenu: [
-      {
-        path: "/login",
-        icon: <ArrowRightOnRectangleIcon className={submenuIconClasses} />,
-        name: "Login",
-      },
-      {
-        path: "/register", //url
-        icon: <UserIcon className={submenuIconClasses} />, // icon component
-        name: "Register", // name that appear in Sidebar
-      },
-      {
-        path: "/forgot-password",
-        icon: <KeyIcon className={submenuIconClasses} />,
-        name: "Forgot Password",
-      },
-      {
-        path: "/app/blank",
-        icon: <DocumentIcon className={submenuIconClasses} />,
-        name: "Blank Page",
-      },
-      {
-        path: "/app/404",
-        icon: <ExclamationTriangleIcon className={submenuIconClasses} />,
-        name: "404",
-      },
-    ],
-  },
-  {
-    path: "", //no url needed as this has submenu
     icon: <Cog6ToothIcon className={`${iconClasses} inline`} />, // icon component
     name: "Settings", // name that appear in Sidebar
     submenu: [
@@ -95,28 +63,6 @@ const routes = [
         path: "/app/settings-team", // url
         icon: <UsersIcon className={submenuIconClasses} />, // icon component
         name: "Team Members", // name that appear in Sidebar
-      },
-    ],
-  },
-  {
-    path: "", //no url needed as this has submenu
-    icon: <DocumentTextIcon className={`${iconClasses} inline`} />, // icon component
-    name: "Documentation", // name that appear in Sidebar
-    submenu: [
-      {
-        path: "/app/getting-started", // url
-        icon: <DocumentTextIcon className={submenuIconClasses} />, // icon component
-        name: "Getting Started", // name that appear in Sidebar
-      },
-      {
-        path: "/app/features",
-        icon: <TableCellsIcon className={submenuIconClasses} />,
-        name: "Features",
-      },
-      {
-        path: "/app/components",
-        icon: <CodeBracketSquareIcon className={submenuIconClasses} />,
-        name: "Components",
       },
     ],
   },
